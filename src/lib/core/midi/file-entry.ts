5e0761261eab511072d45db7c630b6530f7ac732
Hannah Park <hannah@crescent.dev>
2025-07-29 14:49:21 +0900

    change default value of `isSustainVisible`

--- src/lib/core/midi/file-entry.ts
+++ src/lib/core/midi/file-entry.ts
@@ -23,7 +23,7 @@ export function createMidiFileEntry(
     parsedData,
     isVisible: true,
     isPianoRollVisible: true,
-    isSustainVisible: true,
+    isSustainVisible: false,
     fileColor: color,
     color,
     isMuted: false,
